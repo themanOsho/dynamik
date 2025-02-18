b6697ddfed423dea8fecf3627e3c6a43fc051eef
Joshua Osho <tlkjorsh@icloud.com>
2025-02-19 00:55:37 +0100

    Added WooCommerce support: Styles, product gallery features, and wrapper structure

---
+++ woocommerce.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * WooCommerce Compatibility File for Dynamik Theme
+ * 
+ * @package Dynamik
+ */
+
+/**
+ * Declare WooCommerce support.
+ */
+function dynamik_woocommerce_setup() {
+    add_theme_support( 'woocommerce' );
+    add_theme_support( 'wc-product-gallery-zoom' );
+    add_theme_support( 'wc-product-gallery-lightbox' );
+    add_theme_support( 'wc-product-gallery-slider' );
+}
+add_action( 'after_setup_theme', 'dynamik_woocommerce_setup' );
+
+/**
+ * Enqueue WooCommerce styles.
+ */
+function dynamik_woocommerce_styles() {
+    if ( class_exists( 'WooCommerce' ) ) {
+        wp_enqueue_style( 'dynamik-woocommerce', get_template_directory_uri() . '/woocommerce/woocommerce.css', array(), wp_get_theme()->get( 'Version' ) );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'dynamik_woocommerce_styles' );
+
+/**
+ * Modify WooCommerce wrapper structure.
+ */
+function dynamik_before_woocommerce_wrapper() {
+    echo '<div class="container woocommerce-wrapper">';
+}
+add_action( 'woocommerce_before_main_content', 'dynamik_before_woocommerce_wrapper', 10 );
+
+function dynamik_after_woocommerce_wrapper() {
+    echo '</div>'; // Close .woocommerce-wrapper
+}
+add_action( 'woocommerce_after_main_content', 'dynamik_after_woocommerce_wrapper', 10 );
