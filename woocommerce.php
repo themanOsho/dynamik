cc3aba134e0d3e7244c01a50050e7c40e8c9ca61
Joshua Osho <tlkjorsh@icloud.com>
2025-02-19 01:07:19 +0100

    Updated WooCommerce stylesheet enqueue to use filemtime() for cache busting

--- woocommerce.php
+++ woocommerce.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * WooCommerce Compatibility File for Dynamik Theme
- * 
+ *
  * @package Dynamik
  */
 
@@ -9,10 +9,10 @@
  * Declare WooCommerce support.
  */
 function dynamik_woocommerce_setup() {
-    add_theme_support( 'woocommerce' );
-    add_theme_support( 'wc-product-gallery-zoom' );
-    add_theme_support( 'wc-product-gallery-lightbox' );
-    add_theme_support( 'wc-product-gallery-slider' );
+	add_theme_support( 'woocommerce' );
+	add_theme_support( 'wc-product-gallery-zoom' );
+	add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-slider' );
 }
 add_action( 'after_setup_theme', 'dynamik_woocommerce_setup' );
 
@@ -20,9 +20,10 @@ add_action( 'after_setup_theme', 'dynamik_woocommerce_setup' );
  * Enqueue WooCommerce styles.
  */
 function dynamik_woocommerce_styles() {
-    if ( class_exists( 'WooCommerce' ) ) {
-        wp_enqueue_style( 'dynamik-woocommerce', get_template_directory_uri() . '/woocommerce/woocommerce.css', array(), wp_get_theme()->get( 'Version' ) );
-    }
+	if ( class_exists( 'WooCommerce' ) ) {
+		$css_path = get_template_directory() . '/assets/css/woocommerce.css';
+		wp_enqueue_style( 'dynamik-woocommerce', get_template_directory_uri() . '/assets/css/woocommerce.css', array(), filemtime( $css_path ) );
+	}
 }
 add_action( 'wp_enqueue_scripts', 'dynamik_woocommerce_styles' );
 
@@ -30,11 +31,11 @@ add_action( 'wp_enqueue_scripts', 'dynamik_woocommerce_styles' );
  * Modify WooCommerce wrapper structure.
  */
 function dynamik_before_woocommerce_wrapper() {
-    echo '<div class="container woocommerce-wrapper">';
+	echo '<div class="container woocommerce-wrapper">';
 }
 add_action( 'woocommerce_before_main_content', 'dynamik_before_woocommerce_wrapper', 10 );
 
 function dynamik_after_woocommerce_wrapper() {
-    echo '</div>'; // Close .woocommerce-wrapper
+	echo '</div>'; // Close .woocommerce-wrapper
 }
 add_action( 'woocommerce_after_main_content', 'dynamik_after_woocommerce_wrapper', 10 );
